d9e13bc3b6df745481d7afc1f5ca93a69744dc60
MacGregor Thompson <mthompson@gofetchweb.com>
2016-06-29 09:07:34 -0400

    foo

---
+++ PracticeProjects/Babel/gulpfile.js
@@ -0,0 +1,8 @@
+/**
+ * Created by MacGregorThompson on 6/29/16.
+ */
+var gulp = require('gulp');
+
+gulp.task('default', function() {
+    
+});
---
+++ PracticeProjects/Babel/js/gofetch.js
@@ -0,0 +1,4 @@
+/**
+ * Created by MacGregorThompson on 6/29/16.
+ */
+[1,2,3].map(n => n+1);
